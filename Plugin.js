c11952206d4607a007be2243e7124e252b419671
Matthias Giger <i@matthiasgiger.com>
2020-07-21 15:43:16 +0200

    feat(webpack-plugin): add webpack plugin
    Add a LocalDependenciesPlugin for webpack that installs listed dependencies and triggers the watcher on file changes.

--- Plugin.js
+++ Plugin.js
@@ -1,1 +1,64 @@
-// webpack plugin to integrate with watcher.
+import { join } from 'path'
+import { readFileSync } from 'fs'
+import validateOptions from 'schema-utils'
+import pacote from 'pacote'
+// import chokidar from 'chokidar'
+
+const schema = {
+  title: 'LocalDependenciesPlugin options',
+  type: 'object',
+  properties: {
+    production: {
+      description: 'Enables the plugin also in production mode, default false.',
+      type: 'boolean',
+    },
+    watch: {
+      description:
+        'Watches packages for changes and updates on changes, default true.',
+      type: 'boolean',
+    },
+  },
+}
+
+export const LocalDependenciesPlugin = class {
+  constructor(options = { production: false, watch: true }) {
+    validateOptions(schema, options, { name: 'LocalDependenciesPlugin' })
+    this.production = options.production
+    this.watch = options.watch
+  }
+
+  apply(compiler) {
+    if (compiler.options.mode === 'production' && !this.production) {
+      return
+    }
+
+    const { localDependencies } = JSON.parse(
+      readFileSync(join(process.cwd(), 'package.json'), 'utf8')
+    )
+
+    if (!localDependencies || Object.keys(localDependencies).length < 1) {
+      return
+    }
+
+    Object.keys(localDependencies).forEach((name) => {
+      const packagePath = localDependencies[name]
+      console.log(`synec: Installing ${name} from ${packagePath}`)
+
+      console.log(join(process.cwd(), 'node_modules', name))
+      pacote
+        .extract(packagePath, join(process.cwd(), 'node_modules', name))
+        .then(({ from, resolved, integrity }) => {
+          console.log('extracted!', from, resolved, integrity)
+        })
+    })
+
+    compiler.hooks.watchRun.tapAsync(
+      'LocalDependenciesPlugin',
+      (_compiler, done) => {
+        console.log('WATCH RUN')
+
+        return done()
+      }
+    )
+  }
+}
--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-#!/usr/bin/env node
-
-// Entry in package.json "localDependencies"
-
-// Build project dist
-
-// Copy over project "files"
-
-// Install missing dependencies
-
-// Watch for changes in "files"
-
-// Watch for changes to "dependencies" in package.json
+// Webpack Plugin
+export { LocalDependenciesPlugin } from './Plugin.js'
